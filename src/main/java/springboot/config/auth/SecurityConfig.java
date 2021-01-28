87963deea1a742368ab0851df17258218bd1b472
moongomi <kyrdudn2@naver.com>
2021-01-28 20:28:32 +0900

    oauth 로그인 완성

--- src/main/java/springboot/config/auth/SecurityConfig.java
+++ src/main/java/springboot/config/auth/SecurityConfig.java
@@ -1,7 +1,7 @@
 package springboot.config.auth;
 
 import lombok.RequiredArgsConstructor;
-import org.h2.engine.Role;
+import springboot.domain.user.Role;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,14 +12,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().headers().frameOptions().disable()
-                .and().authorizeRequests().antMatchers("/","/css/**","/images/**",
-                "/js/**","/h2-console/**").permitAll()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
-                .and().logout().logoutSuccessUrl("/")
-                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 
 }
